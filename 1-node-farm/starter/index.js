d00f6dc2504863630af59d12206b9d552801ff79
Ogaga <iyaraogaga69@gmail.com>
2023-01-12 07:15:15 +0100

    Use slugify

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
 
 //FILES
@@ -44,6 +44,9 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => { return slugify(el.productName, { lower: true }) })
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true)
