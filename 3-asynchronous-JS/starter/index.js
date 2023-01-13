06dc4e880a0e814516cd9d2ea6df2093a6fa2d13
Ogaga <iyaraogaga69@gmail.com>
2023-01-13 14:44:30 +0100

    Multiple promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -33,12 +33,27 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
 
-    await writeFilePro(`dog-img.txt`, res.body.message);
+    const res2pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1pro, res2pro, res3pro]);
+    console.log(all);
+    const img = all.map((el) => {
+      return el.body.message;
+    });
+    console.log(img);
+    // console.log(res.body.message);
+
+    await writeFilePro(`dog-img.txt`, img.join("\n"));
     console.log("Save dog image file");
   } catch (err) {
     console.log(err);
