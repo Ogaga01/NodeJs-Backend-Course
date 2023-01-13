7150e16c0716a45e268ae0061d2b9719f2d97556
Ogaga <iyaraogaga69@gmail.com>
2023-01-13 12:47:15 +0100

    promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -6,13 +6,15 @@ fs.readFile(`${__dirname}/dog.txt`, function (err, data) {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile(`dog-img.txt`, res.body.message, (err) => {
+        if (err) return console.log(err.message);
         console.log("Save dog image file");
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
