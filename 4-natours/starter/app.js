1fd4feebd41449c3bec90820c4e98759f0402718
Ogaga <iyaraogaga69@gmail.com>
2023-01-17 17:28:51 +0100

    Handling post request

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
 // })
@@ -24,6 +26,26 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newID = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({ id: newID }, req.body)
+    
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if (err) {
+            res.status(500).json({
+            });
+        }
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        })
+    })
+})
 
 const port = 3000;
 
