f67dc7100ff186b95b72fdb30639777bf15fab4b
Ogaga <iyaraogaga69@gmail.com>
2023-01-18 09:04:12 +0100

    Responding to URL Parameters

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
@@ -19,33 +19,57 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-      status: 'success',
-      results: tours.length,
+    status: 'success',
+    results: tours.length,
     data: {
       tours,
     },
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  //   if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tour.length,
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body)
-    const newID = tours[tours.length - 1].id + 1
-    const newTour = Object.assign({ id: newID }, req.body)
-    
-    tours.push(newTour)
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        if (err) {
-            res.status(500).json({
-            });
-        }
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour,
-            },
-        })
-    })
-})
+  // console.log(req.body)
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({});
+      }
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 
