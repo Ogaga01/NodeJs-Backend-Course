723b47a3b8bd14a1d31984862f88a4b475c5d283
Ogaga <iyaraogaga69@gmail.com>
2023-01-18 13:17:52 +0100

    Serving Static files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,6 +9,7 @@ const app = express();
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
