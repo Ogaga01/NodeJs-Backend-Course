a543533a500d4e2974016e6e6454205533cfa021
Ogaga <iyaraogaga69@gmail.com>
2023-01-19 14:00:53 +0100

    Creating A simple Model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -20,6 +20,24 @@ mongoose
     console.log('DB connection successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+})
+  
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
