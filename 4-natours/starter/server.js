3828b3ad6e57f891360135110c85304cdc410552
Ogaga <iyaraogaga69@gmail.com>
2023-01-19 14:27:39 +0100

    Creating documents and testing the model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -32,11 +32,25 @@ const tourSchema = new mongoose.Schema({
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
-})
-  
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 
