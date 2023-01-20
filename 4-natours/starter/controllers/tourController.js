605c80ea2b7d238ec77d3dc04f4e9ebd99e3dbce
Ogaga <iyaraogaga69@gmail.com>
2023-01-20 09:06:08 +0100

    Creating documents using the mongoDB

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -15,15 +15,15 @@ const Tour = require('./../models/tourModel')
 //     next();
 // }
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'Missing name or price',
-      });
-    }
-    next();
-}
+// exports.checkBody = (req, res, next) => {
+//     if (!req.body.name || !req.body.price) {
+//       return res.status(400).json({
+//         status: 'fail',
+//         message: 'Missing name or price',
+//       });
+//     }
+//     next();
+// }
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -73,12 +73,20 @@ exports.deleteTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
   // console.log(req.body)
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status:'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
