fac34429fb2725ff55497cec61e8074f7b774a69
Ogaga <iyaraogaga69@gmail.com>
2023-01-20 12:29:47 +0100

    Importing Development Data

---
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import Data into DB
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data imported successfully');
+    }
+    catch (err) {
+        console.log(err);
+    }
+    process.exit()
+}
+
+// Delete All Data
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data deleted successfully');
+    }
+    catch (err) {
+        console.log(err);
+    }
+    process.exit()
+}
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] === '--delete') {
+    deleteData();
+}
+
+// Delete Data
+// const deleteData = async (id) => {
+//     try {
+//         await Tour.deleteOne({ _id: id });
+//         console.log('Data deleted successfully');
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+console.log(process.argv)
