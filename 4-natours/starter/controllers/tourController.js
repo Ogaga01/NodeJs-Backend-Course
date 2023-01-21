8656fb40196120f4d5c853c52c354b9ee8b4b577
Ogaga <iyaraogaga69@gmail.com>
2023-01-21 12:42:56 +0100

    Refactoring API Features

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -14,47 +14,6 @@ exports.aliasTopTours = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //Build Query
-    //1a. Filtering
-    // const queryObj = { ...req.query }
-    // const excludedFields = ['page', 'sort', 'limit', 'fields']
-    // excludedFields.forEach(el => delete queryObj[el])
-
-    // //1b. Advanced Filtering
-    // let queryStr = JSON.stringify(queryObj)
-    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-
-    // let query = Tour.find(JSON.parse(queryStr));
-
-    //2. Sorting
-    // if (req.query.sort) {
-    //   const sortBy = req.query.sort.split(',').join(' ');
-    //   query = query.sort(sortBy);
-    // } else {
-    //   query = query.sort('-createdAt');
-    // }
-
-    //3. Field Limiting
-    // if (req.query.fields) {
-    //   const fields = req.query.fields.split(',').join(' ');
-    //   query = query.select(fields);
-    // } else {
-    //   query = query.select('-__v');
-    // }
-
-    //4. Pagination
-    // const page = req.query.page * 1 || 1;
-    // const limit = req.query.limit * 1 || 10;
-    // const skip = (page - 1) * limit;
-    // query = query.skip(skip).limit(limit);
-
-    // if (req.query.page) {
-    //   const numTours = await Tour.countDocuments();
-    //   if (skip >= numTours) {
-    //     throw new Error ('This page does not exist')
-    //   }
-    // }
-
     //Execute Query
     const features = new APIFeatures(Tour.find(), req.query)
       .filter()
@@ -132,7 +91,6 @@ exports.deleteTour = async (req, res) => {
 };
 
 exports.createTour = async (req, res) => {
-  // console.log(req.body)
   try {
     const newTour = await Tour.create(req.body);
     res.status(201).json({
@@ -148,3 +106,14 @@ exports.createTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
+}
