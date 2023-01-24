b98bcc93e36cd5f906d37e3ebf9f548a2f29378c
Ogaga <iyaraogaga69@gmail.com>
2023-01-24 09:20:49 +0100

    Handling Unhandles Routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -36,5 +31,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    satus: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+})
 
 module.exports = app;
