f70c3273e62c4e7395cf19ca5dc1ddf75ed8dfd9
Ogaga <iyaraogaga69@gmail.com>
2023-01-25 07:58:16 +0100

    Managing Passwords

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
-userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   //  name, email, photo, password, password confirm
-
   name: {
     type: String,
     required: [true, 'Please tell us your name'],
@@ -14,10 +14,10 @@ userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide an email email'],
-      unique: true,
+    unique: true,
     lowercase: true,
-      trim: true,
-    validate: [validator.isEmail, 'Please provide a valid email']
+    trim: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
   password: {
@@ -31,9 +31,28 @@ userSchema = new mongoose.Schema({
     required: [true, 'Please confirm your password'],
     trim: true,
     minLength: [8, 'A user password must have more than 8 characters'],
+    validate: {
+      // This only works on create and save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
 });
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
