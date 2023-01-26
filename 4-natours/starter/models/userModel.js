391171e0a23646bf8e19fb8a967c7f874da3f62b
Ogaga <iyaraogaga69@gmail.com>
2023-01-26 09:20:50 +0100

    Postman Advanced Setup

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -68,7 +68,7 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
       this.passwordChangedAt.getTime() / 1000,
       10
     );
-        console.log(changedTimestamp, JWTTimestamp)
+
     return JWTTimestamp < changedTimestamp;
   }
 
