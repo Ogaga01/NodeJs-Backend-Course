cfb1892beae67f5712c9c96eadfadd515af269d2
Ogaga <iyaraogaga69@gmail.com>
2023-02-03 11:05:54 +0100

    Data Sanitization

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-const rateLimit = require('express-rate-limit')
-const helmet = require('helmet')
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const app = express();
 
 // Middleware
-app.use(helmet())
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -20,12 +21,14 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests sent. Please try again in one hour'
-})
+  message: 'Too many requests sent. Please try again in one hour',
+});
 
-app.use('/api', limiter)
+app.use('/api', limiter);
 
-app.use(express.json({limit: '10kb'}));
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize())
+app.use(xss())
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
