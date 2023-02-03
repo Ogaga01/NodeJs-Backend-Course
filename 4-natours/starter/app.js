f0657d744080fb9fb7a60252748e21ca330ccffd
Ogaga <iyaraogaga69@gmail.com>
2023-02-03 09:46:07 +0100

    Setting Security HTTP Headers

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,6 +11,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // Middleware
+app.use(helmet())
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -22,7 +25,7 @@ const limiter = rateLimit({
 
 app.use('/api', limiter)
 
-app.use(express.json());
+app.use(express.json({limit: '10kb'}));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
