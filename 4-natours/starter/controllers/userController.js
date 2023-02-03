839036237807cbacbd569db50466e79209409caa
Ogaga <iyaraogaga69@gmail.com>
2023-02-03 08:26:29 +0100

    Updating the Current User: Data

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -1,28 +1,48 @@
 const User = require('./../models/userModel');
-const AppError = require('./../utils/appError')
-const catchAsyncError = require('./../utils/catchAsyncError')
+const AppError = require('./../utils/appError');
+const catchAsyncError = require('./../utils/catchAsyncError');
 
-exports.getAllUsers = catchAsyncError( async (req, res, next) => {
-  const users = await User.find()
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj
+}
 
-    // Send Response
-    res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-        users,
-      },
-    });
+exports.getAllUsers = catchAsyncError(async (req, res, next) => {
+  const users = await User.find();
+
+  // Send Response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
 });
 
-exports.updateMe = (req, res, next) => {
+exports.updateMe = catchAsyncError(async (req, res, next) => {
   //1. Throw error if user tries to update password
   if (req.body.password || req.body.passwordConfirm) {
-    return next(new AppError(`Can't update password with this route`, 400))
+    return next(new AppError(`Can't update password with this route`, 400));
   }
 
   //2. Update user document
-}
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  })
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
