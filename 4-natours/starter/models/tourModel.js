dcadf56e0176d5678ba3cd9b3fa5c7517d66fca5
Ogaga <iyaraogaga69@gmail.com>
2023-02-04 12:53:47 +0100

    Modelling Tour Guides: Embedding

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,6 +101,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -116,6 +118,14 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async(id) => {
+    return await User.findById(id)
+  })
+  this.guides = await Promise.all(guidesPromises)
+  next()
+})
 
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
