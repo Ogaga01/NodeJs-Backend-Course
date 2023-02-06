0380a05dc0d4b7fee357a077d94294b287dbca45
Ogaga <iyaraogaga69@gmail.com>
2023-02-06 13:48:43 +0100

    Implementing Simple Nested Routes

--- 4-natours/starter/controllers/reviewController.js
+++ 4-natours/starter/controllers/reviewController.js
@@ -38,6 +38,9 @@ exports.getReview = catchAsyncError(async (req, res, next) => {
 });
 
 exports.createReview = catchAsyncError(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
