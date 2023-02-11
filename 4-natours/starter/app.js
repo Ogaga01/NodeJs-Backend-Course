01fcd0cb5711e17a7acd818ddbcc94aa17e1d71b
Ogaga <iyaraogaga69@gmail.com>
2023-02-11 16:06:02 +0100

    Setting up the Project Structure

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,6 +7,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const viewRouter = require('./routes/viewRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
@@ -57,25 +58,7 @@ app.use((req, res, next) => {
 // app.post('/api/v1/tours', createTour);
 
 // Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
-  })
-})
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
-  });
-});
-
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter)
