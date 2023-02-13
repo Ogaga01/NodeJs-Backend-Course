859bd5a41e42ca22fbd33ce8de842201a1dbf495
Ogaga <iyaraogaga69@gmail.com>
2023-02-13 13:49:26 +0100

    Logging in Users with Our API - Part 3

--- 4-natours/starter/public/js/mapbox.js
+++ 4-natours/starter/public/js/mapbox.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations)
-console.log(locations)
 
+export const displayMap = (locations) => {
 mapboxgl.accessToken =
   'pk.eyJ1Ijoib2dhZ2EwMSIsImEiOiJjbGUxaWU0dHEwbmViM25wNWllYnBuMjdvIn0.t_mX1x5kV3DzVZy1T13ccw';
 var map = new mapboxgl.Map({
@@ -11,36 +10,38 @@ var map = new mapboxgl.Map({
 
 const bounds = new mapboxgl.LngLatBounds();
 
-  locations.forEach(loc => {
-    // Create marker
-    const el = document.createElement('div');
-    el.className = 'marker';
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
 
-    // Add marker
-    new mapboxgl.Marker({
-      element: el,
-      anchor: 'bottom'
-    })
-      .setLngLat(loc.coordinates)
-      .addTo(map);
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
 
-    // Add popup
-    new mapboxgl.Popup({
-      offset: 30
-    })
-      .setLngLat(loc.coordinates)
-      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-      .addTo(map);
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
 
-    // Extend map bounds to include current location
-    bounds.extend(loc.coordinates);
-  });
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
 
-  map.fitBounds(bounds, {
-    padding: {
-      top: 200,
-      bottom: 150,
-      left: 100,
-      right: 100
-    }
-  });
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});  
+}
+
