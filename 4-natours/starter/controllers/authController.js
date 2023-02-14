b8d14f738c0c6a3f83dee77cf08f7699d29fbfd6
Ogaga <iyaraogaga69@gmail.com>
2023-02-14 13:35:26 +0100

    Logging in Users with Our API - Part 3

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -68,6 +68,14 @@ exports.login = catchAsyncError(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'logged out', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  })
+  res.status(200).json({status: 'success'})
+}
+
 exports.protect = catchAsyncError(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
@@ -107,28 +115,33 @@ exports.protect = catchAsyncError(async (req, res, next) => {
   next();
 });
 
-exports.isLoggedIn = catchAsyncError(async (req, res, next) => {
-  if (req.cookies.jwt) {
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
+exports.isLoggedIn = async (req, res, next) => {
+  try {
 
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.id);
-    if (!currentUser) {
-      return next();
-    }
-
-    // 4) Check if user changed password after the token was issued
-    if (currentUser.changedPasswordAfter(decoded.iat)) {
-      return next();
-    }
+    if (req.cookies.jwt) {
+      // 2) Verification token
+      const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
   
-    // GRANT ACCESS TO PROTECTED ROUTE
-    res.locals.user = currentUser
-    return next();
+      // 3) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+  
+      // 4) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+    
+      // GRANT ACCESS TO PROTECTED ROUTE
+      res.locals.user = currentUser
+      return next();
+    }
+  } catch (err) {
+    return next()
   }
   next()
-});
+};
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
